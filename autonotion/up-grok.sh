b3140edd47d0f6b858ff5c57e0b42fb7906e0e40
RegularUs3r <78124142+RegularUs3r@users.noreply.github.com>
2024-07-26 13:02:11 -0300

    Update up-grok.sh

--- autonotion/up-grok.sh
+++ autonotion/up-grok.sh
@@ -2,7 +2,7 @@
 
 
 ngrok http 80 > /dev/null & 
-python3 -m http.server 80 --directory /media/sf_SANTANDER/SPRINTS/Sprint-2 &
+python3 -m http.server 80 --directory /media/<SOME-SECRET-FOLDER>/Sprint-2 &
 sleep 1 && ng=$(curl -sS http://localhost:4040/api/tunnels | jq -r .tunnels[0].public_url | awk -F// '{print $2}')
 echo "Here's your ngrok address $ng"
 
